7b9081cc8cf240194c299e407fd0d7baaae61e97
yankoshbadal <109698057+yankoshbadal@users.noreply.github.com>
2026-09-04 16:42:46 +0530

    implement myPosts

---
+++ src/routes/profile/myPosts.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const auth = require("../../middlewares/auth");
+const Posts = require("../../models/posts");
+
+const myPostRouter = express.Router();
+
+myPostRouter.get("/myPost", auth, async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+
+    const allPosts = await Posts.find({
+      author: loggedInUserId,
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Posts fetched successfully",
+      posts: allPosts,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+      error: error.message,
+    });
+  }
+});
+
+module.exports = myPostRouter;
